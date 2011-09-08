ff4bcd17b093c2f99356c2413511b88523ef5c4a
Kevin Elliott <kevin@welikeinc.com>
2011-09-08 00:16:07 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+gem 'json'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# To use debugger
+# gem 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Placemonkey::Application.config.secret_token = '08212bfe40c15196c8b15b663a846b6b195cacc00d3399ddaf2e973f54d7d4b0f433551159cc0c17a3bbbc19b8ec03fa67865ebebe74d6e2a4c9a75a2291420b'
